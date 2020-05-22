67a3138f378b1af80cbb82921989600b83017bec
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-05-21 21:21:37 -0400

    js

---
+++ assets/Javascript/script.js
@@ -0,0 +1,7 @@
+var numberButtons = document.querySelectorAll('[data-number]');
+var operationButtons = document.querySelectorAll('[data-operation]');
+var equalsButton = document.querySelector('[data-equals]');
+var deleteButton = document.querySelector('[data-equals]');
+var allClearButton = document.querySelector('[data-equals]');
+var previousOperandTextElement = document.querySelector('[data-equals]');
+var equalsButton = document.querySelector('[data-equals]');
