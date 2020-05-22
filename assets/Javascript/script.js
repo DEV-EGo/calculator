6b12466f4381740ca5a474d15cab4efb5212f482
edwin gonzalez <edwin.gonzalez11@hotmail.com>
2020-05-22 13:21:04 -0400

    operand v2

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -22,6 +22,9 @@ class Calculator {
 
     chooseOperation(operation) {
         if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
         this.operation = operation
         this.previousOperand = this.currentOperand
         this.currentOperand = ''
@@ -44,8 +47,8 @@ const operationButtons = document.querySelectorAll('[data-operation]');
 const equalsButton = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
-const previousOperandTextElement = document.querySelector('[data-previous-operand]');
-const currentOperandTextElelement = document.querySelector('[data-current-operand]');
+const previousOperandTextElement = document.querySelector('[data-previous-Operand]');
+const currentOperandTextElelement = document.querySelector('[data-current-Operand]');
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElelement);
 
